c8d7acdfe47d8476e07d2f04cb873ca5063a0977
AlexPegg <alexpeggsailing@gmail.com>
2022-11-13 12:28:40 +0000

    close port, select baudrate

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,17 +4,22 @@ import Layout from "../../components/Layout";
 
 
 const Home = () => {
+
+	const [baudRate, setBaudRate] = useState(9600);
+	var keepReading: boolean
+
 	let port: any, reader: any, log: any, writer: any
-	let lineReader: any = undefined
 	const loadSerialPorts = async () => {
 		if ('serial' in navigator) {
 			try {
+				//need to close all ports
 				port = await navigator.serial.requestPort();
-				await port.open({ baudRate: 115200, bufferSize: 1024, dataBits: 8, parity: "none", stopBits: 1 });
-				lineReader = port.readable
+				await port.open({ baudRate: baudRate, bufferSize: 1024, dataBits: 8, parity: "none", stopBits: 1 });
+				reader = port.readable
 					.pipeThrough(new TextDecoderStream())
 					.pipeThrough(new TransformStream(new LineBreakTransformer()))
 					.getReader();
+				keepReading = true
 				monitorPort()
 			}
 			catch (err) {
@@ -25,6 +30,27 @@ const Home = () => {
 			console.error('The Web serial API not enabled in your browser.');
 		}
 	}
+
+	const removeSerialPorts = async () => {
+		if (keepReading == true) { //don't close if not open
+			keepReading = false
+			console.log(keepReading)
+			const textEncoder = new TextEncoderStream();
+			const writer = textEncoder.writable.getWriter();
+			const writableStreamClosed = textEncoder.readable.pipeTo(port.writable);
+			const textDecoder = new TextDecoderStream();
+			const readableStreamClosed = port.readable.pipeTo(textDecoder.writable);
+
+			reader.cancel();
+			await readableStreamClosed.catch(() => { /* Ignore the error */ });
+
+			writer.close();
+			await writableStreamClosed;
+
+			await port.close();
+		}
+	}
+
 
 	class LineBreakTransformer {
 		container: string;
@@ -49,19 +75,17 @@ const Home = () => {
 
 
 	const monitorPort = async () => {
-		while (port.readable) {
+		while (port.readable && keepReading) {
 			//const textDecoder = new TextDecoderStream();
 			try {
 				while (true) {
-					const { value, done } = await lineReader.read()
+					const { value, done } = await reader.read()
 					if (done) {
-						// Allow the serial port to be closed later.
 						reader.releaseLock()
 
 						break
 					}
 					if (value) {
-						console.log(value)
 						addMessage(value)
 					}
 				}
@@ -69,6 +93,7 @@ const Home = () => {
 				// TODO: Handle non-fatal read error.
 			}
 		}
+		console.log("closing port")
 	}
 
 	const addMessage = (message: string) => {
@@ -82,6 +107,7 @@ const Home = () => {
 		}
 		div.className = 'message title3'
 		Terminal.appendChild(div);
+		Terminal.scrollTop = Terminal.scrollHeight //scroll with content
 	}
 
 	const clearAllMessages = () => {
@@ -103,7 +129,32 @@ const Home = () => {
 
 				</div>
 				<div className="flex flex-col w-2/12 bg-white border-l-4 border-blue-600">
-					<button onClick={loadSerialPorts} className="flex">Connect to Port</button>
+					<div>
+						<h2> Port Settings</h2>
+						<div>
+							<h3>Baud Rate</h3>
+							<select
+								value={baudRate}
+								onChange={(e) => {
+									setBaudRate(parseInt(e.target.value));
+								}}
+							>
+								<option value={9600}> 9600 </option>
+								<option value={115200}> 115200 </option>
+								<option value={0}> Custom </option>
+
+							</select>
+
+						</div>
+						<button onClick={loadSerialPorts} className="flex">Connect Port</button>
+						<button onClick={removeSerialPorts} className="flex">Disconnect Port</button>
+					</div>
+					<div>
+						<h2> Receive Settings</h2>
+					</div>
+					<div>
+						<h2> Send Settings</h2>
+					</div>
 					<button onClick={clearAllMessages} className="flex" >Clear</button>
 				</div>
 			</div>
