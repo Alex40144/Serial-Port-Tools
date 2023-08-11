4da409a269c5a8062eeeb10f4b246fe8787ea398
AlexPegg <alexpeggsailing@gmail.com>
2023-08-11 14:59:26 +0100

    spotify redirect dynamic.

--- src/pages/SpotifyAuth.tsx
+++ src/pages/SpotifyAuth.tsx
@@ -2,12 +2,15 @@ import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import querystring from 'querystring';
 import { Buffer } from 'buffer';
+import type { GetServerSidePropsContext, GetServerSideProps } from 'next'
 
 const SPOTIFY_AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize';
 const SPOTIFY_TOKEN_ENDPOINT = 'https://accounts.spotify.com/api/token';
 const CLIENT_ID = 'e1ef6e76db754951ac5e97f222cc58b5';
-const REDIRECT_URI = 'http://localhost:3000/SpotifyAuth';
 const SCOPES = ['user-read-currently-playing'];
+
+let REDIRECT_URI = '';
+let host = ''
 
 function base64URLEncode(buffer: Uint8Array) {
     return Buffer.from(buffer).toString('base64')
@@ -26,6 +29,7 @@ async function generateCodeChallenge(codeVerifier: string) {
 }
 
 async function getToken(code: string | string[]) {
+    console.log('getting token')
     const codeVerifier = sessionStorage.getItem('code_verifier');
     const body = {
         client_id: CLIENT_ID,
@@ -42,14 +46,17 @@ async function getToken(code: string | string[]) {
         body: querystring.stringify(body)
     });
     const data = await response.json();
-    sessionStorage.setItem('access_token', data.access_token);
+    console.log(data)
+    console.log(sessionStorage.getItem('access_token'))
+    if (sessionStorage.getItem('access_token') == 'undefined' || sessionStorage.getItem('access_token') == null || sessionStorage.length < 1) {
+        sessionStorage.setItem('access_token', data.access_token);
+    }
 }
 
 async function authorize() {
     const codeVerifier = base64URLEncode(crypto.getRandomValues(new Uint8Array(32)));
     const codeChallenge = await generateCodeChallenge(codeVerifier);
     sessionStorage.setItem('code_verifier', codeVerifier);
-
     const params = {
         client_id: CLIENT_ID,
         response_type: 'code',
@@ -62,9 +69,18 @@ async function authorize() {
     window.location.href = url;
 }
 
-export default function SpotifyAuth() {
-    const router = useRouter();
+export const getServerSideProps = async (
+    context: GetServerSidePropsContext
+) => {
+    const { req } = context;
+    let url = req.headers.host;
+    return { props: { url } }
+}
 
+export default function SpotifyAuth({ url }: { url: string }) {
+    const router = useRouter();
+    REDIRECT_URI = 'http://' + url + '/SpotifyAuth'
+    console.log(REDIRECT_URI)
     useEffect(() => {
         if (!router.isReady) return;
         console.log(router.query)
